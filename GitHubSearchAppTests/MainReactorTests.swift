64ca6a4b69e1f8d1abf37b0909a327dc52f239f3
김동준 <corykim0829@gmail.com>
2025-01-05 22:21:59 +0900

    테스트 함수명 수정 및 실패 케이스 테스트 추가

--- GitHubSearchAppTests/MainReactorTests.swift
+++ GitHubSearchAppTests/MainReactorTests.swift
@@ -12,7 +12,11 @@ import RxSwift
 
 final class MainReactorTests: XCTestCase {
 	
-	func testSearchAction() throws {
+	enum GitHubSearchAPIMockError: Error {
+		case responseStatus403Error
+	}
+	
+	func test_search_action이_들어올_때_검색이_성공한_경우_현재키워드와_레포지토리이름을_업데이트합니다() throws {
 		// Given
 		let mockAPI = GitHubSearchAPIMock()
 		let sut = MainReactor(searchAPI: mockAPI)
@@ -24,9 +28,24 @@ final class MainReactorTests: XCTestCase {
 		// Then
 		XCTAssertEqual(sut.currentState.currentKeyword, "TEST")
 		XCTAssertEqual(sut.currentState.repositoryNames.count, 2)
+		XCTAssertNil(sut.currentState.error)
 	}
 	
-	func testfetchingNextPage() throws {
+	func test_search_action이_들어올_때_검색이_실패한_경우_레포지토리이름은_빈_배열이고_에러를_업데이트합니다() throws {
+		// Given
+		let mockAPI = GitHubSearchAPIMock()
+		let sut = MainReactor(searchAPI: mockAPI)
+		mockAPI.searchResultStub = .error(GitHubSearchAPIMockError.responseStatus403Error)
+		
+		// When
+		sut.action.onNext(.search("TEST"))
+		
+		// Then
+		XCTAssertEqual(sut.currentState.repositoryNames, [])
+		XCTAssertNotNil(sut.currentState.error)
+	}
+	
+	func test_fetchNextPage_action이_들어올_때_다음_페이지_조회가_성공한_경우_현재키워드를_업데이트하고_레포지토리_이름을_기존레포지토리이름에_추가합니다() throws {
 		// Given
 		let mockAPI = GitHubSearchAPIMock()
 		let sut = MainReactor(searchAPI: mockAPI)
@@ -40,6 +59,24 @@ final class MainReactorTests: XCTestCase {
 		// Then
 		XCTAssertEqual(sut.currentState.currentKeyword, "TEST")
 		XCTAssertEqual(sut.currentState.repositoryNames.count, 3)
+		XCTAssertNil(sut.currentState.error)
+	}
+	
+	func test_fetchNextPage_action이_들어올_때_다음_페이지_조회가_실패한_경우_현재키워드를_업데이트하고_레포지토리_이름을_이전_응답_데이터를_유지합니다() throws {
+		// Given
+		let mockAPI = GitHubSearchAPIMock()
+		let sut = MainReactor(searchAPI: mockAPI)
+		mockAPI.searchResultStub = .just((["TEST1", "TEST2"], 1)) // Mock API 에서 검색 결과 2개 반환하고 있음
+		sut.action.onNext(.search("TEST"))
+		mockAPI.searchResultStub = .error(GitHubSearchAPIMockError.responseStatus403Error)
+		
+		// When
+		sut.action.onNext(.fetchNextPage)
+		
+		// Then
+		XCTAssertEqual(sut.currentState.currentKeyword, "TEST")
+		XCTAssertEqual(sut.currentState.repositoryNames.count, 2)
+		XCTAssertNotNil(sut.currentState.error)
 	}
 	
 }
