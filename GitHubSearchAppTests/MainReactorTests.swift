062f1d726a2fabc7ca17b3807c65b8434a6fe3b3
김동준 <corykim0829@gmail.com>
2025-01-05 22:04:13 +0900

    APIMock이 stub 반환 구조 변경에 따른 테스트 코드 수정

--- GitHubSearchAppTests/MainReactorTests.swift
+++ GitHubSearchAppTests/MainReactorTests.swift
@@ -15,7 +15,8 @@ final class MainReactorTests: XCTestCase {
 	func testSearchAction() throws {
 		// Given
 		let mockAPI = GitHubSearchAPIMock()
-		let sut = MainReactor(searchAPI: mockAPI) // Mock API 에서 검색 결과 2개 반환하고 있음
+		let sut = MainReactor(searchAPI: mockAPI)
+		mockAPI.searchResultStub = .just((["TEST1", "TEST2"], 1)) // Mock API 에서 검색 결과 2개 반환하고 있음
 		
 		// When
 		sut.action.onNext(.search("TEST"))
@@ -28,15 +29,17 @@ final class MainReactorTests: XCTestCase {
 	func testfetchingNextPage() throws {
 		// Given
 		let mockAPI = GitHubSearchAPIMock()
-		let sut = MainReactor(searchAPI: mockAPI) // Mock API 에서 검색 결과 2개 반환하고 있음
+		let sut = MainReactor(searchAPI: mockAPI)
+		mockAPI.searchResultStub = .just((["TEST1", "TEST2"], 1)) // Mock API 에서 검색 결과 2개 반환하고 있음
+		sut.action.onNext(.search("TEST"))
+		mockAPI.searchResultStub = .just((["TEST3"], nil)) // 다음 search 요청에서는 검색 결과 1개 더 추가한다
 		
 		// When
-		sut.action.onNext(.search("TEST"))
 		sut.action.onNext(.fetchNextPage)
 		
 		// Then
 		XCTAssertEqual(sut.currentState.currentKeyword, "TEST")
-		XCTAssertEqual(sut.currentState.repositoryNames.count, 4)
+		XCTAssertEqual(sut.currentState.repositoryNames.count, 3)
 	}
 	
 }
