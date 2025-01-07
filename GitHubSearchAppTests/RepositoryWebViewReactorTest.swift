f8ce5057aa748275adf4f0844cba81d04a872c06
김동준 <corykim0829@gmail.com>
2025-01-07 20:36:24 +0900

    RepsitoryWebViewReactorTest 생성 및 .loadView 테스트 생성

---
+++ GitHubSearchAppTests/RepositoryWebViewReactorTest.swift
@@ -0,0 +1,27 @@
+//
+//  RepositoryWebViewReactorTest.swift
+//  GitHubSearchAppTests
+//
+//  Created by Cory Kim on 1/7/25.
+//
+
+import XCTest
+
+import RxSwift
+
+@testable import GitHubSearchApp
+
+final class RepositoryWebViewReactorTest: XCTestCase {
+	
+	func test_loadView_action이_실행되면_레포지토리_이름에_맞는_URL을_만든다() throws {
+		// Given
+		let sut = RepositoryWebViewReactor()
+			
+		// When
+		sut.action.onNext(.loadView(repositoryName: "swiftlang/swift"))
+		
+		// Then
+		XCTAssertEqual(sut.currentState.currentURL?.absoluteString, "https://github.com/swiftlang/swift")
+	}
+	
+}
