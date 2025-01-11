c60fb98bfb461306b5899f649f831d86797ce91a
김동준 <corykim0829@gmail.com>
2025-01-11 15:24:59 +0900

    refactor: 테스트 파라미터 상수화

--- GitHubSearchAppTests/RepositoryWebViewReactorTest.swift
+++ GitHubSearchAppTests/RepositoryWebViewReactorTest.swift
@@ -16,12 +16,13 @@ final class RepositoryWebViewReactorTest: XCTestCase {
 	func test_loadView_action이_실행되면_레포지토리_이름에_맞는_URL을_만든다() throws {
 		// Given
 		let sut = RepositoryWebViewReactor()
+		let dummyRepositoryName = "swiftlang/swift"
 			
 		// When
-		sut.action.onNext(.loadView(repositoryName: "swiftlang/swift"))
+		sut.action.onNext(.loadView(repositoryName: dummyRepositoryName))
 		
 		// Then
-		XCTAssertEqual(sut.currentState.currentURL?.absoluteString, "https://github.com/swiftlang/swift")
+		XCTAssertEqual(sut.currentState.currentURL?.absoluteString, "https://github.com/" + dummyRepositoryName)
 	}
 	
 }
